c5f6e83088bb81499393a60afaa654ea021063b6
keke856 <54512054+keke856@users.noreply.github.com>
2021-03-23 23:32:32 -0300

    v4

--- Gerenciador de Cursos/src/Controller/Alterar.php
+++ Gerenciador de Cursos/src/Controller/Alterar.php
@@ -1,14 +1,43 @@
 <?php
 namespace Alura\Cursos\Controller;
-use Alura\Cursos\Controller\InterfaceCotroladorRequisicao;
 
-require_once "../../vendor/autoload.php";
+
+
+use Alura\Cursos\Controller\InterfaceCotroladorRequisicao;
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+require_once __DIR__."/../../vendor/autoload.php";
+
 
 class Alterar implements InterfaceCotroladorRequisicao{
 
+    private $repository;
+
+    public function __construct()
+    {
+      $entityManager = (new EntityManagerCreator())->getEntityManager();  
+      $this->repository= $entityManager->getRepository(Curso::class);
+    
+    }
+
     public function processarRequisicao(): void
     {
-        $id = filter_input(INPUT_GET,'id',FILTER_VALIDATE_INT);
-        echo $id;
+       
+       
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT);
+    
+        if (is_null($id) || $id === false) {
+            header('Location: /listar-cursos');
+        }
+
+       $cursos = $this->repository->find($id); 
+        $titulo ="Alteração de Cursos ".$cursos->getDescricao();
+        require_once __DIR__."/../../view/cadastro-curso.php";
+
+  
     }
 }
--- Gerenciador de Cursos/view/cadastro-curso.php
+++ Gerenciador de Cursos/view/cadastro-curso.php
@@ -4,10 +4,10 @@
             
             <form action='/public/salvar-dados' method='post'>
                 <div class="form-grup">
-                    <input class="form-control w-25" type="text" name="descricao" placeholder="curso...">
+                    <input class="form-control w-25" type="text" name="descricao" placeholder="curso..." value="<?= isset($cursos)?$cursos->getDescricao():''; ?>">
                 </div>
                 <button class="btn btn-primary mt-2">Salvar</button>
             </form>
-         
+          
          
 <?php include_once "fraguimento-front/fim.php";       
